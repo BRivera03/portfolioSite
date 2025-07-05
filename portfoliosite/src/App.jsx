154ea1bd93279bf30c10a7af8fa73e51b119e791
BRivera03 <brivera@edgewood.edu>
2025-07-04 23:02:22 -0500

    Finish css setup
    Created utils.js file to store image function for components.
    
    Added fonts and colors as variables in the css files

--- portfoliosite/src/App.jsx
+++ portfoliosite/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import './App.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className={StyleSheet.App}>
       Hello World!
-    </>
+    </div>
   )
 }
 
